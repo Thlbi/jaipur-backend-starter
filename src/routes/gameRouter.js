1db0bd76e130b3ef64e2792f0b1d2e39a82f4886
thlbi <thibaudleroy99@gmail.com>
2021-10-04 01:27:28 +0200

    test

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -5,11 +5,12 @@ const router = express.Router()
 
 // Listen to POST /games
 router.post("/", function (req, res) {
-    if (!req.body.name) {
-      return res.status(400).send("Missing name parameter")
-    }
-    const newGame = gameService.createGame(req.body.name)
-    res.status(201).json(newGame)
-  })
+  if (!req.body.name) {
+    return res.status(400).send("Missing name parameter")
+  }
+  const newGame = gameService.createGame(req.body.name)
+  res.status(201).json(newGame)
+})
 
-  export default router
+export default router
+// test
